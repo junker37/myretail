4915727c219e2ecc8acf8a1f1bb5c9db2a6a54ba
Jeremy McJunkin <jeremy@doapps.com>
2015-04-03 21:38:47 -0500

    ProductsResource junit created

---
+++ maven/webservices/src/test/java/com/myretail/api/resource/ProductsResourceTest.java
@@ -0,0 +1,58 @@
+/**
+ * ProductsResourceTest.java
+ * myRetail-webservices
+ * 
+ * Created by jeremy on Apr 3, 2015
+ * DoApp, Inc. owns and reserves all rights to the intellectual
+ * property and design of the following application.
+ *
+ * Copyright 2015 - All rights reserved.  Created by DoApp, Inc.
+ */
+package com.myretail.api.resource;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author jeremy
+ *
+ */
+public class ProductsResourceTest {
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {}
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @AfterClass
+  public static void tearDownAfterClass() throws Exception {}
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @Before
+  public void setUp() throws Exception {}
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @After
+  public void tearDown() throws Exception {}
+
+  /**
+   * Test method for {@link com.myretail.api.resource.ProductsResource#getProduct(java.lang.Integer)}.
+   */
+  @Test
+  public void testGetProduct() {
+    fail("Not yet implemented");
+  }
+
+}
