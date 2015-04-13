8c0abd22e15779b62120e15016e43cb92a099730
junker37 <junker37@gmail.com>
2015-04-12 20:58:48 -0500

    build first if necessary

--- scripts/start_webserver.sh
+++ scripts/start_webserver.sh
@@ -3,5 +3,13 @@
 # Starts the webserver, any arguments passed in are passed along to the java command
 
 JAVA_PROPS=$@
+
+if [ ! -e ../maven/webservices/target/webservices-0.0.1-SNAPSHOT.jar ];
+then
+  echo "Building..."
+  cd ../maven
+  mvn clean package
+fi
+
 echo "Starting webserver with java args $JAVA_PROPS"
 java $JAVA_PROPS -cp ../maven/webservices/target/webservices-0.0.1-SNAPSHOT.jar com.myretail.api.app.MyRetailAPIApplication server
